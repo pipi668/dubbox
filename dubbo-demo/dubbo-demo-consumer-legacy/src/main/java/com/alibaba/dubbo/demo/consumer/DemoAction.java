a96d038a763d21c005cc4bf72b15bf67649736d6
Bugs <pleasantboy@qq.com>
2017-07-03 22:25:45 +0800

    submit uncommited

--- dubbo-demo/dubbo-demo-consumer-legacy/src/main/java/com/alibaba/dubbo/demo/consumer/DemoAction.java
+++ dubbo-demo/dubbo-demo-consumer-legacy/src/main/java/com/alibaba/dubbo/demo/consumer/DemoAction.java
@@ -21,23 +21,24 @@ import java.util.Date;
 import com.alibaba.dubbo.demo.DemoService;
 
 public class DemoAction {
-    
-    private DemoService demoService;
 
-    public void setDemoService(DemoService demoService) {
-        this.demoService = demoService;
-    }
+	private DemoService demoService;
+
+	public void setDemoService(DemoService demoService) {
+		this.demoService = demoService;
+	}
 
 	public void start() throws Exception {
-        for (int i = 0; i < Integer.MAX_VALUE; i ++) {
-            try {
-            	String hello = demoService.sayHello("world" + i);
-                System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] " + hello);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Thread.sleep(2000);
-        }
+		System.out.println("Begin first action.");
+		for (int i = 0; i < 1000; i++) {
+			try {
+				String hello = demoService.sayHello("world" + i);
+				System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] " + hello);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Thread.sleep(2000);
+		}
 	}
 
 }
